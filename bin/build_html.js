eccb059c54affaeb1cc90e68f6bb977e343c3048
wate <pc.poisoning@gmail.com>
2023-05-05 02:48:42 +0900

    サイトマップの出力内容を微調整

--- bin/build_html.js
+++ bin/build_html.js
@@ -306,7 +306,7 @@ ${error.message}
       return url;
     });
     const sitemapTemplateString = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9/">
   {% for url in urlList -%}
   <url>
     <loc>{{ url.loc }}</loc>
