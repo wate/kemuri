3697edf1145766289002b3541ae68f3ab1313295
wate <pc.poisoning@gmail.com>
2023-11-03 15:56:10 +0900

    サーバー機能を調整

--- lib/server.ts
+++ lib/server.ts
@@ -9,6 +9,19 @@ import * as dotenv from 'dotenv';
 import './console';
 dotenv.config();
 
+const argv = yargs(process.argv.slice(2))
+  .options({
+    w: { type: 'boolean', default: true, alias: 'watch', description: 'watchモードの指定' },
+    p: { type: 'number', default: 3000, alias: 'port', description: 'ポート番号' },
+    open: { type: 'boolean', default: true, description: '起動時にブラウザを開く' },
+    notify: { type: 'boolean', default: false, description: '更新通知の表示' },
+    'base-dir': { type: 'string', default: 'public', description: 'ベースディレクトリ' },
+    'watch-files': { type: 'array', description: '監視対象ファイル' },
+    ui: { type: 'boolean', default: false, description: 'UI機能を利用' },
+    'ui-port': { type: 'boolean', default: false, alias: 'p', description: 'UI機能のポート番号' },
+  })
+  .parseSync();
+
 interface serverOption {
   baseDir?: string;
   port?: number;
@@ -21,40 +34,119 @@ interface serverOption {
 }
 
 const serverOption = configLoader.getServerOption();
-let htmlOutputDir = 'public';
-if (!configLoader.isDisable('html')) {
-  const htmlOption = configLoader.getHtmlOption();
-  if (_.has(htmlOption, 'outputDir') && _.get(htmlOption, 'outputDir')) {
-    //@ts-ignore
-    htmlOutputDir = _.get(htmlOption, 'outputDir');
+/**
+ * browserSyncのベースディレクトリ
+ */
+let browserSyncBaseDir: string = 'public';
+if (argv.baseDir !== undefined) {
+  browserSyncBaseDir = argv.baseDir;
+} else {
+  if (!configLoader.isEnable('html')) {
+    const htmlOption = configLoader.getHtmlOption();
+    if (_.has(htmlOption, 'outputDir')) {
+      //@ts-ignore
+      browserSyncBaseDir = _.get(htmlOption, 'outputDir');
+    }
   }
 }
+/**
+ * browserSyncのUIオプション
+ */
+let browserSyncPort: number = 3000;
+if (argv.port !== undefined) {
+  //@ts-ignore
+  browserSyncPort = argv.port;
+} else if (_.has(serverOption, 'port')) {
+  //@ts-ignore
+  browserSyncPort = _.get(serverOption, 'port');
+}
+/**
+ * browserSyncのwatchオプション
+ */
+let browserSyncWatch: boolean = true;
+if (argv.watch !== undefined) {
+  //@ts-ignore
+  browserSyncWatch = argv.watch;
+} else if (_.has(serverOption, 'watch')) {
+  //@ts-ignore
+  browserSyncWatch = _.get(serverOption, 'watch');
+}
+/**
+ * browserSyncの監視対象ファイルのオプション
+ */
+let browserSyncWatchFiles: string | string[] | boolean = false;
+if (browserSyncWatch) {
+  if (argv.watchFiles !== undefined) {
+    //@ts-ignore
+    browserSyncWatchFiles = argv.watchFiles;
+  } else if (_.has(serverOption, 'watchFiles')) {
+    //@ts-ignore
+    browserSyncWatchFiles = _.get(serverOption, 'watchFiles');
+  }
+}
+
+/**
+ * browserSyncのブラウザopenのオプション
+ */
+let browserSyncOpen: boolean = true;
+if (argv.open !== undefined) {
+  browserSyncOpen = argv.open;
+} else if (_.has(serverOption, 'open')) {
+  //@ts-ignore
+  browserSyncOpen = _.get(serverOption, 'open');
+}
+
+/**
+ * browserSyncのUIオプション
+ */
+
 let browserSyncUIOption: boolean | browserSync.UIOptions = false;
-if (_.has(serverOption, 'ui') && _.get(serverOption, 'ui')) {
+if (argv.ui !== undefined) {
+  browserSyncUIOption = argv.ui;
+} else if (_.has(serverOption, 'ui') && _.get(serverOption, 'ui')) {
   browserSyncUIOption = true;
-  if (_.has(serverOption, 'uiPort') && _.get(serverOption, 'uiPort')) {
+}
+/**
+ * browserSyncのUIポート番号
+ */
+if (browserSyncUIOption) {
+  if (argv.uiPort !== undefined) {
+    browserSyncUIOption = {
+      //@ts-ignore
+      port: argv.uiPort,
+    };
+  } else if (_.has(serverOption, 'uiPort')) {
     browserSyncUIOption = {
       port: _.get(serverOption, 'uiPort'),
     };
   }
 }
+
 /**
- * browserSyncのオプション
+ * browserSyncの通知オプション
  */
+let browserSyncNotify: boolean = false;
+if (argv.notify !== undefined) {
+  browserSyncNotify = argv.notify;
+} else if (_.has(serverOption, 'notify')) {
+  //@ts-ignore
+  browserSyncNotify = _.get(serverOption, 'notify');
+}
+
 const browserSyncOption: browserSync.Options = {
   server: {
-    baseDir: _.has(serverOption, 'baseDir') ? _.get(serverOption, 'baseDir') : htmlOutputDir,
+    baseDir: browserSyncBaseDir,
   },
-  port: _.has(serverOption, 'port') ? _.get(serverOption, 'port') : 3000,
-  watch: _.has(serverOption, 'watch') ? _.get(serverOption, 'watch') : true,
+  port: browserSyncPort,
+  watch: browserSyncWatch,
   //@ts-ignore
-  files: _.has(serverOption, 'watchFiles') ? _.get(serverOption, 'watchFiles') : false,
-  open: _.has(serverOption, 'open') ? _.get(serverOption, 'open') : true,
+  files: browserSyncWatchFiles,
+  open: browserSyncOpen,
   ui: browserSyncUIOption,
-  notify: _.has(serverOption, 'notify') ? _.get(serverOption, 'notify') : false,
+  notify: browserSyncNotify,
 };
-console.group(chalk.blue('Server Option'));
-console.log(serverOption);
+console.group(chalk.blue('browserSync Server Option'));
+console.log(browserSyncOption);
 console.groupEnd();
 
 browserSync(browserSyncOption);
