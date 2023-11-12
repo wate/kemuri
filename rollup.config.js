b0411298d51ea8c49ab4009a4b8e28aca732f73a
wate <pc.poisoning@gmail.com>
2023-11-12 17:56:53 +0900

    微調整

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: {
+    builder: 'lib/builder.ts',
     'html-builder': 'lib/html-builder.ts',
     'css-builder': 'lib/css-builder.ts',
     'js-builder': 'lib/js-builder.ts',
-    builder: 'lib/builder.ts',
     screenshot: 'lib/screenshot.ts',
     'snippet-builder': 'lib/snippet-builder.ts',
   },
