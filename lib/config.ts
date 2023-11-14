c521fc48c8f75d814e018dc060e31f9deb658d13
wate <pc.poisoning@gmail.com>
2023-11-15 02:08:02 +0900

    サーバー関連の環境変数のパース処理を実装

--- lib/config.ts
+++ lib/config.ts
@@ -58,25 +58,145 @@ class configLoader {
       _.cloneDeep(screenshotConfig),
     );
   }
-  public static parseEnableEnv(): object {
-    return {};
+  /**
+   * 定義済みの環境変数のキーを取得する
+   * @param envNamePrefix
+   * @returns
+   */
+  protected static getEnvkeys(envNamePrefix: string): Array<string> {
+    return Object.keys(process.env)
+      .filter((key) => key.startsWith(envNamePrefix))
+      .map((key) => key.replace(envNamePrefix, ''));
   }
+  /**
+   * 環境変数の値を配列に変換する
+   * @param envValue
+   * @returns
+   */
+  protected static convertEnvValueToArray(envValue: string, toLowerCase: boolean = false): Array<string> {
+    return envValue
+      .split(',')
+      .map((item) => {
+        if (toLowerCase) {
+          return item.toLowerCase().trim();
+        } else {
+          return item.trim();
+        }
+      })
+      .filter((item) => item.length > 0)
+      .reduce((unique: string[], current: string) => {
+        return unique.includes(current) ? unique : [...unique, current];
+      }, []);
+  }
+
+  /**
+   * 環境変数の値をbooleanに変換する
+   * @param envValue
+   * @returns
+   */
+  protected static convertEnvValueToBool(envValue: string): boolean {
+    return ['true', 'yes', 'on', '1'].includes(envValue.toLocaleLowerCase());
+  }
+  /**
+   * 環境変数の値を数値に変換する
+   * @param envValue
+   * @returns
+   */
+  protected static convertEnvValueToInt(envValue: string): Number | null {
+    return envValue.length > 0 ? parseInt(envValue, 10) : null;
+  }
+  /**
+   * 環境変数に設定された有効化するビルダーの一覧を取得する
+   * @returns
+   */
+  public static parseEnableEnv(): Array<string> {
+    //@ts-ignore
+    return _.has(process.env, 'KEMURI_ENABLE') ? configLoader.convertEnvValueToArray(process.env.KEMURI_ENABLE) : [];
+  }
+
+  /**
+   * 環境変数に設定されたサーバーの設定を取得する
+   * @returns
+   */
   public static parseServerEnv(): object {
-    return {};
+    const settings: any = {};
+    const settingKeys = configLoader.getEnvkeys('KEMURI_SERVER_');
+    if (settingKeys.includes('BASE_DIR')) {
+      //@ts-ignore
+      settings.baseDir = configLoader.convertEnvValueToArray(process.env.KEMURI_SERVER_BASE_DIR);
+    }
+    if (settingKeys.includes('PORT')) {
+      //@ts-ignore
+      settings.port = configLoader.convertEnvValueToInt(process.env.KEMURI_SERVER_PORT);
+    }
+    if (settingKeys.includes('WATCH')) {
+      //@ts-ignore
+      settings.watch = configLoader.convertEnvValueToBool(process.env.KEMURI_SERVER_WATCH);
+    }
+    if (settingKeys.includes('WATCH_FILES')) {
+      //@ts-ignore
+      settings.watchFiles = configLoader.convertEnvValueToArray(process.env.KEMURI_SERVER_WATCH_FILES);
+    }
+    if (settingKeys.includes('OPEN')) {
+      //@ts-ignore
+      settings.open = configLoader.convertEnvValueToBool(process.env.KEMURI_SERVER_OPEN);
+    }
+    if (settingKeys.includes('BROWSER')) {
+      //@ts-ignore
+      settings.browser = process.env.KEMURI_SERVER_BROWSER;
+    }
+    if (settingKeys.includes('NOTIFY')) {
+      //@ts-ignore
+      settings.notify = configLoader.convertEnvValueToBool(process.env.KEMURI_SERVER_NOTIFY);
+    }
+    if (settingKeys.includes('UI')) {
+      //@ts-ignore
+      settings.ui = configLoader.convertEnvValueToBool(process.env.KEMURI_SERVER_UI);
+    }
+    if (settingKeys.includes('UI_PORT')) {
+      //@ts-ignore
+      settings.uiPort = configLoader.convertEnvValueToInt(process.env.KEMURI_SERVER_UI_PORT);
+    }
+    return settings;
   }
+  /**
+   * 環境変数に設定されたHTMLビルダーの設定を取得する
+   * @returns
+   */
   public static parseHtmlEnv(): object {
+    const settingKeys = configLoader.getEnvkeys('KEMURI_HTML_');
     return {};
   }
+  /**
+   * 環境変数に設定されたJSビルダーの設定を取得する
+   * @returns
+   */
   public static parseJsEnv(): object {
+    const settingKeys = configLoader.getEnvkeys('KEMURI_JS_');
     return {};
   }
+  /**
+   * 環境変数に設定されたCSSビルダーの設定を取得する
+   * @returns
+   */
   public static parseCssEnv(): object {
+    const settingKeys = configLoader.getEnvkeys('KEMURI_CSS_');
     return {};
   }
+  /**
+   * 環境変数に設定されたスニペットビルダーの設定を取得する
+   * @returns
+   */
   public static parseSnippetEnv(): object {
+    const settingKeys = configLoader.getEnvkeys('KEMURI_SNIPPET_');
     return {};
   }
+  /**
+   * 環境変数に設定されたスクリーンショットの設定を取得する
+   * @returns
+   */
   public static parseScreenshotEnv(): object {
+    const settingKeys = configLoader.getEnvkeys('KEMURI_SCREENSHOT_');
     return {};
   }
   /**
