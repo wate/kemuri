f5b378ea83ff19dafcaa40a500caae3576468733
wate <pc.poisoning@gmail.com>
2023-11-19 16:13:31 +0900

    update

--- lib/builder.ts
+++ lib/builder.ts
@@ -27,8 +27,9 @@ const argv = yargs(process.argv.slice(2))
     js: { type: 'boolean', description: 'jsビルダーを利用する' },
     server: { type: 'boolean', description: 'browserSyncサーバーを起動する' },
     c: { type: 'string', alias: 'config', description: '設定ファイルを指定する' },
-    init: { type: 'boolean', description: '設定ファイルを生成する' },
+    init: { type: 'boolean', description: 'プロジェクトの初期設定を行う' },
     force: { type: 'boolean', default: false, alias: 'f', description: '設定ファイルを強制的に上書きする' },
+    configOnly: { type: 'boolean', default: false, description: '設定ファイルのみを出力する' },
   })
   .parseSync();
 
@@ -43,6 +44,9 @@ if (argv.init) {
   } else {
     configLoader.copyDefaultConfig(argv.force);
     console.log(chalk.green('Configuration file(.builderrc.yml) has been generated.'));
+  }
+  if(argv.configOnly){
+    process.exit(0);
   }
   const createDirectories: string[] = [];
   const htmlBuilderOption = configLoader.getHtmlOption();
