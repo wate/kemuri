6d6e2582b029fdd4916a953bf07f3b0721ce0cf9
wate <pc.poisoning@gmail.com>
2023-12-24 13:24:36 +0900

    実行前に出力先ディレクトリをクリーンアップする処理を追加

--- lib/snippet-builder.ts
+++ lib/snippet-builder.ts
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 import snippetBuilder from './builder/snippet';
 import configLoader from './config';
+import * as path from 'node:path';
+import * as glob from 'glob';
 import chalk from 'chalk';
+import fs from 'fs-extra';
 import yargs from 'yargs';
 
 const argv = yargs(process.argv.slice(2))
   .options({
     w: { type: 'boolean', default: false, alias: 'watch', description: 'watchモードの指定' },
     c: { type: 'string', alias: 'config', description: '設定ファイルを指定する' },
+    clean: { type: 'boolean', default: false, description: 'ビルド前に出力ディレクトリのスニペットファイルを削除する' },
   })
   .parseSync();
 
@@ -21,6 +25,16 @@ console.group(chalk.blue('Builder Option'));
 console.log(builderOption);
 console.groupEnd();
 snippetBuilder.setOption(builderOption);
+
+if (argv.clean) {
+  console.group(chalk.yellow('Clean up snippet files'));
+  const removeFilePattern = path.join(snippetBuilder.getOutputDir(), '*.' + snippetBuilder.getOutputExt());
+  glob.sync(removeFilePattern).forEach((removeFile) => {
+    console.log('Remove file: ' + removeFile);
+    fs.removeSync(removeFile);
+  });
+  console.groupEnd();
+}
 
 snippetBuilder.buildAll();
 
